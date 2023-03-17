fae70ee2c0bb383c677896ba11cb685a1003f67c
Sikri <user@users-MacBook-Pro.local>
2023-03-17 18:19:19 +0530

    pom file version changes, dependencies and spring boot set up

---
+++ src/main/java/com/monklabs/monkpay/MonkpayApplication.java
@@ -0,0 +1,13 @@
+package com.monklabs.monkpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monklabs/monkpay/MonkpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monklabs.monkpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonkpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
